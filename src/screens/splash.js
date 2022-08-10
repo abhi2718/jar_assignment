2426578ef0a1c1116883ee0ea15f01f39afc8d34
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Pro.local>
2022-08-10 11:27:49 +0530

    redux and navigation setup

---
+++ src/screens/splash.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View,Text,SafeAreaView} from 'react-native';
+
+export const SplashScreen=()=>{
+    return(
+        <SafeAreaView>
+          <View>
+           <Text>splash screen</Text>
+          </View>
+        </SafeAreaView>
+    );
+}
---
+++ src/store/actions/resource_action/res_action.js
@@ -0,0 +1,37 @@
+export const REQ_FOR_LOAD_RESOURCE = 'REQ_FOR_LOAD_RESOURCE';
+export const REQ_FOR_LOAD_RESOURCE_SUCCESS = 'REQ_FOR_LOAD_RESOURCE_SUCCESS';
+export const REQ_FOR_LOAD_RESOURCE_FAIL = 'RREQ_FOR_LOAD_RESOURCE_FAIL';
+import {BASE_URL} from '../index';
+
+export const getResourceAction =
+    (pageNumber) =>
+     async (dispatch) => {
+      dispatch({type: REQ_FOR_LOAD_RESOURCE});
+      try {
+        const response = await fetch(`${BASE_URL}=${pageNumber}`,{
+            method: 'GET',
+            headers: new Headers({
+              'Content-Type': 'application/json',
+            }),
+          });
+          const data = await response.json();
+          console.log('data: ',data);
+          if (response.ok){
+             dispatch({
+                type: REQ_FOR_LOAD_RESOURCE_SUCCESS,
+                payload: {...data},
+               });
+          }else{
+            dispatch({
+                type: REQ_FOR_LOAD_RESOURCE_FAIL,
+                payload: {...data},
+               });
+          }
+      } catch (error) {
+          console.log(error);
+          dispatch({
+            type: REQ_FOR_LOAD_RESOURCE_FAIL,
+            payload: {...error},
+           });
+      }
+    };
