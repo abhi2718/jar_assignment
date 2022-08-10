8056a68a73b8193d4bff2c0311b34cae29e6d720
Abhishek Singh <abhisheksingh@Abhisheks-MacBook-Pro.local>
2022-08-10 08:43:26 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+     <View>
+      <Text>Welcome</Text>
+     </View>
+    </SafeAreaView>
+  );
+};
+
+
+export default App;
